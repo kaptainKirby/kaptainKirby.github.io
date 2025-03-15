03bef21ccdcf8a4fee29bc5778434724f5ca251c
kaptainKirby <rherwig@mail.bradley.edu>
2025-03-15 18:15:55 -0500

    null check 2.0

--- ToyCookies.cs
+++ ToyCookies.cs
@@ -79,7 +79,7 @@ function DeleteCookie(name)
 function SetCookieText(name, expirationDate)
 {
     var value = "null";
-    if (!document.getElementById("message").value)
+    if (document.getElementById("message").value != null || document.getElementById("message").value != "")
         value = document.getElementById("message").value;
     console.log(value);
     CreateCookie(name, value, expirationDate);
